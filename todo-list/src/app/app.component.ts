2676d0de2552a50f151fec4d4c20b523210665d7
Vladimir Gulev <vladinson009@icloud.com>
2025-04-10 23:12:31 +0200

    added one more component

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from "./product-card/product-card.component";
 import { CounterComponent } from './counter/counter.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductCardComponent, CounterComponent],
+  imports: [RouterOutlet, ProductCardComponent, CounterComponent, MyComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
