6b8e6a54c60278996005f1f8984e9c1c6b461a2b
Vladimir Gulev <vladinson009@icloud.com>
2025-04-12 03:03:49 +0200

    added contact form

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductCardComponent } from "./product-card/product-card.component";
+import { ProductCardComponent } from './product-card/product-card.component';
 import { CounterComponent } from './counter/counter.component';
 import { MyComponentComponent } from './my-component/my-component.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductCardComponent, CounterComponent, MyComponentComponent],
+  imports: [
+    RouterOutlet,
+    ProductCardComponent,
+    CounterComponent,
+    MyComponentComponent,
+    ContactFormComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'My First App';
