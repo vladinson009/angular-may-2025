af687ffa8ad153d3178b2d9d6ace83cff53e4c75
Vladimir Gulev <vladinson009@icloud.com>
2025-04-22 15:03:41 +0200

    constants

--- playground/src/app/constants.ts
+++ playground/src/app/constants.ts
@@ -1,4 +1,8 @@
-import { AuthEndpoints, DataEndpoints } from './types/Constants';
+import {
+  AuthEndpoints,
+  DataEndpoints,
+  LikesEndpoints,
+} from './types/Constants';
 const baseUrl: string = 'http://localhost:3030';
 const authUrl: string = baseUrl + '/users';
 
@@ -10,6 +14,15 @@ export const AUTH_ENDPOINTS: AuthEndpoints = {
 export const DATA_ENDPOINTS: DataEndpoints = {
   getAll: baseUrl + '/data/tattoos?sortBy=_createdOn%20desc',
   gallery: baseUrl + '/data/tattoos',
+};
+export const LIKES_ENDPOINTS: LikesEndpoints = {
+  likes: baseUrl + '/data/likes',
+  getLikes: (tattooId: string) =>
+    baseUrl +
+    `/data/likes?where=tattooId%3D%22${tattooId}%22&distinct=_ownerId&count`,
+  isLiked: (tattooId: string, userId: string) =>
+    baseUrl +
+    `/data/likes?where=tattooId%3D%22${tattooId}%22%20and%20_ownerId%3D%22${userId}%22&count`,
 };
 
 export const AUTH_DATA: string = 'authData';
