5e6690634261429c0906fe30140ee9d3d6cdf2af
Vladimir Gulev <vladinson009@icloud.com>
2025-04-22 15:03:59 +0200

    added interceptor for Authorizated requests

--- playground/src/app/app.config.ts
+++ playground/src/app/app.config.ts
@@ -2,18 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { AuthInterceptor } from './services/authInterceptor.service';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
