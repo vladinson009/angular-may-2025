05981f601573528ffbfea6bab443e07df080e6dc
Vladimir Gulev <vladinson009@icloud.com>
2025-04-25 15:45:50 +0200

    dynamic data in catalogue

--- GamesPlay/games-play/src/app/components/catalogue/catalogue.component.ts
+++ GamesPlay/games-play/src/app/components/catalogue/catalogue.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { GalleryShape } from '../../types/Data';
+import { RouterLink } from '@angular/router';
+import { LoadingSpinnerComponent } from '../../loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-catalogue',
-  imports: [],
+  imports: [RouterLink, LoadingSpinnerComponent],
   templateUrl: './catalogue.component.html',
-  styleUrl: './catalogue.component.css'
+  styleUrl: './catalogue.component.css',
 })
-export class CatalogueComponent {
+export class CatalogueComponent implements OnInit {
+  #dataService = inject(DataService);
+  #fetchData() {
+    return this.#dataService.getAll().subscribe({
+      next: (data) => {
+        this.items = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.isLoading = false;
+      },
+    });
+  }
+  items: GalleryShape[] = [];
+  isLoading: boolean = true;
+  error: string | null = null;
 
+  ngOnInit(): void {
+    this.#fetchData();
+  }
 }
