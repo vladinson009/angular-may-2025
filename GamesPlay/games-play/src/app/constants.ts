ba34425d3dd75c7a19cf191933d5b3ca2a2744bf
Vladimir Gulev <vladinson009@icloud.com>
2025-04-25 15:45:14 +0200

    added collection endpoints

--- GamesPlay/games-play/src/app/constants.ts
+++ GamesPlay/games-play/src/app/constants.ts
@@ -1,7 +1,12 @@
-import { AuthEndpoints, ErrorRequestMsg } from './types/Constants';
+import {
+  AuthEndpoints,
+  ErrorRequestMsg,
+  CollectionEndpoints,
+} from './types/Constants';
 
 const baseUrl: string = 'http://localhost:3030';
 const authUrl: string = baseUrl + '/users';
+const dataUrl: string = baseUrl + '/data';
 
 export const USER_AUTH: string = 'userAuth';
 
@@ -9,6 +14,11 @@ export const AUTH_ENDPOINTS: AuthEndpoints = {
   login: authUrl + '/login',
   logout: authUrl + '/logout',
   register: authUrl + '/register',
+};
+export const COLLECTION_ENDPOINTS: CollectionEndpoints = {
+  catalogue: dataUrl + '/games?sortBy=_createdOn%20desc',
+  recentThree: dataUrl + '/games?sortBy=_createdOn%20desc&distinct=category',
+  collection: dataUrl + '/games',
 };
 
 export const ERROR_REQUEST_MSG: ErrorRequestMsg = {
