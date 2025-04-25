b0ecc5e14ddbb8be5d3d56d0a4e211091f19fa39
Vladimir Gulev <vladinson009@icloud.com>
2025-04-25 15:46:18 +0200

    home dynamic data

--- GamesPlay/games-play/src/app/components/home/home.component.ts
+++ GamesPlay/games-play/src/app/components/home/home.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { GalleryShape } from '../../types/Data';
+import { LoadingSpinnerComponent } from '../../loading-spinner/loading-spinner.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LoadingSpinnerComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  #dataService = inject(DataService);
+  #fetchData() {
+    return this.#dataService.getAll().subscribe({
+      next: (data) => {
+        this.items = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.isLoading = false;
+      },
+    });
+  }
+  items: GalleryShape[] = [];
+  isLoading: boolean = true;
+  error: string | null = null;
+  ngOnInit(): void {
+    this.#fetchData();
+  }
 }
