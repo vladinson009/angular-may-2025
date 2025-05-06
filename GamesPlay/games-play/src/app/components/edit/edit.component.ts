b7eef792600ff5779abab7e6e0105bc77db80ecd
Vladimir Gulev <vladinson009@icloud.com>
2025-05-06 22:55:20 +0200

    implemented edit functionality

--- GamesPlay/games-play/src/app/components/edit/edit.component.ts
+++ GamesPlay/games-play/src/app/components/edit/edit.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DataService } from '../../services/data.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CreateFormShape, GalleryShape } from '../../types/Data';
 
 @Component({
   selector: 'app-edit',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.css'
+  styleUrl: './edit.component.css',
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
+  #route = inject(ActivatedRoute);
+  #router = inject(Router);
+  #fb = inject(FormBuilder);
+  #dataService = inject(DataService);
+  #gameId: string = '';
+  formGroup!: FormGroup;
 
+  constructor() {
+    this.formGroup = this.#fb.group({
+      title: ['', []],
+      category: ['', []],
+      maxLevel: ['', []],
+      imageUrl: ['', []],
+      summary: ['', []],
+    });
+  }
+
+  ngOnInit(): void {
+    this.#route.paramMap.subscribe((params) => {
+      const gameId = params.get('gameId')!;
+      this.#gameId = gameId;
+      this.#dataService.getById(gameId!).subscribe((game) => {
+        this.formGroup.patchValue(game);
+      });
+    });
+  }
+  onEdit() {
+    if (this.formGroup.invalid) {
+    }
+    const body: CreateFormShape = this.formGroup.value;
+    this.#dataService.editById(this.#gameId, body).subscribe(() => {
+      this.#router.navigate(['/games', 'details', this.#gameId]);
+    });
+  }
 }
