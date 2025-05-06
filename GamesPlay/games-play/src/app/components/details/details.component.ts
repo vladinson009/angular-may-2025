bda79727e70a609bc357cd891a354d9f73c69c0b
Vladimir Gulev <vladinson009@icloud.com>
2025-05-06 22:54:44 +0200

    implement details on load data and delete btn

--- GamesPlay/games-play/src/app/components/details/details.component.ts
+++ GamesPlay/games-play/src/app/components/details/details.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { GalleryShape } from '../../types/Data';
+import { DataService } from '../../services/data.service';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { getUserData } from '../../utils/userData';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [RouterLink, LoadingSpinnerComponent],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  #router = inject(Router);
+  #route = inject(ActivatedRoute);
+  #dataService = inject(DataService);
+  item = {} as GalleryShape;
+  isLoading: boolean = true;
+  isOwner: boolean = false;
 
+  ngOnInit(): void {
+    this.#route.paramMap.subscribe((param) => {
+      const gameId: string = param.get('gameId')!;
+      this.#dataService.getById(gameId).subscribe((data) => {
+        this.item = data;
+        this.isOwner = getUserData()?._id == data._ownerId;
+        this.isLoading = false;
+      });
+    });
+  }
+  onDelete(ev: Event) {
+    ev.preventDefault();
+    const confirmation = confirm(
+      `Are you sure you want to delete ${this.item.title}?`
+    );
+    if (!confirmation) {
+      return;
+    }
+    this.#dataService.deleteById(this.item._id).subscribe(() => {
+      this.#router.navigate(['/games', 'catalogue']);
+    });
+  }
 }
