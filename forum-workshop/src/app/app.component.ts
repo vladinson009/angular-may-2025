c13ff5e158bef0bb11d38158df8fb40fa4be79e2
Vladimir Gulev <vladinson009@icloud.com>
2025-05-10 01:40:40 +0200

    added main content

--- forum-workshop/src/app/app.component.ts
+++ forum-workshop/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { AsideComponent } from './aside/aside.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MainComponent, AsideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'forum-workshop';
-}
+export class AppComponent {}
