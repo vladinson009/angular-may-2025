f397451543c52922339a957d5f8cc1a4195a8ae7
Vladimir Gulev <vladinson009@icloud.com>
2025-05-09 23:08:56 +0200

    added header and footer

--- forum-workshop/src/app/app.component.ts
+++ forum-workshop/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'forum-workshop';
