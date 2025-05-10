8e0f541ac535dcc506531492b4e34d0307ce117b
Vladimir Gulev <vladinson009@icloud.com>
2025-05-10 16:31:31 +0200

    updated navbar with additioinal links

--- forum-workshop/src/app/header/header.component.ts
+++ forum-workshop/src/app/header/header.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  #authService = inject(AuthService);
+  isUser: boolean = this.#authService.isLogged;
+  username: string = 'Vladimir';
 }
