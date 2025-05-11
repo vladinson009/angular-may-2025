303764b348c7c5456cb60db02d5bbf9c9f1432d6
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 21:04:07 +0200

    implemented Edit button functionality

--- forum-workshop/src/app/profile/profile.component.ts
+++ forum-workshop/src/app/profile/profile.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
+  #router = inject(Router);
 
+  onEdit() {
+    this.#router.navigate(['profile', 'edit']);
+  }
 }
