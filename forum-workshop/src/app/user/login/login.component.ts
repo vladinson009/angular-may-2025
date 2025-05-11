4a985fc83af6d8bec4e011cdc315ac43f6c9b6ae
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 21:05:50 +0200

    practicing template driven forms

--- forum-workshop/src/app/user/login/login.component.ts
+++ forum-workshop/src/app/user/login/login.component.ts
@@ -1,9 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CreateUserShape, UserShape } from '../../types/User';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  #authService = inject(AuthService);
+
+  loginData = {
+    email: '',
+    password: '',
+  };
+
+  onSubmit(body: CreateUserShape) {
+    this.#authService.login(body).subscribe();
+  }
+}
