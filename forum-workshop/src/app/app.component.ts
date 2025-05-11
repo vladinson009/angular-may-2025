6820f0831613fe21b9cecfa0668d0d65f27f9985
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 21:00:19 +0200

    persist user session

--- forum-workshop/src/app/app.component.ts
+++ forum-workshop/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
-import { AsideComponent } from './aside/aside.component';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, AsideComponent],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  #authService = inject(AuthService);
+  ngOnInit(): void {
+    this.#authService.getToken();
+  }
+}
