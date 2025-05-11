9b788c54be2d1b70258e2d1c01151dead313d9e2
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 15:35:51 +0200

    dynamic theme

--- forum-workshop/src/app/theme-content/theme-content.component.ts
+++ forum-workshop/src/app/theme-content/theme-content.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
+import { ThemesShape } from '../types/Themes';
+import { DatePipe } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-theme-content',
-  imports: [],
+  imports: [DatePipe, HomeComponent],
   templateUrl: './theme-content.component.html',
-  styleUrl: './theme-content.component.css'
+  styleUrl: './theme-content.component.css',
 })
-export class ThemeContentComponent {
-
+export class ThemeContentComponent implements OnInit {
+  #route = inject(ActivatedRoute);
+  #authService = inject(AuthService);
+  #themeService = inject(ThemeService);
+  theme = {} as ThemesShape;
+  isUser: boolean = this.#authService.isLogged;
+  ngOnInit(): void {
+    this.#route.paramMap.subscribe((params) => {
+      const themeId: string | null = params.get('themeId');
+      this.#themeService.fetchThemeById(themeId!).subscribe((theme) => {
+        this.theme = theme;
+      });
+    });
+  }
 }
