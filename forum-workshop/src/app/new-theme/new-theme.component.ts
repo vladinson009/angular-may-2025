4f9f44702d6d0d5ad6e6a5d93566996b5ed9af98
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 15:34:41 +0200

    implemented new theme with reactive forms

--- forum-workshop/src/app/new-theme/new-theme.component.ts
+++ forum-workshop/src/app/new-theme/new-theme.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
+import { CreateThemeShape, ThemesShape } from '../types/Themes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-theme',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-theme.component.html',
-  styleUrl: './new-theme.component.css'
+  styleUrl: './new-theme.component.css',
 })
 export class NewThemeComponent {
+  #fb = inject(FormBuilder);
+  #router = inject(Router);
+  #themeService = inject(ThemeService);
 
+  themeForm: FormGroup;
+  errors = {} as CreateThemeShape;
+  constructor() {
+    this.themeForm = this.#fb.group({
+      title: ['', [Validators.required, Validators.minLength(5)]],
+      post: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit() {
+    this.errors = {} as CreateThemeShape;
+    if (this.themeForm.invalid) {
+      const title = this.themeForm.get('title');
+      const post = this.themeForm.get('post');
+      this.errors = {
+        title: {
+          required: title?.hasError('required'),
+          minlength: title?.hasError('minlength'),
+        },
+        post: {
+          required: post?.hasError('required'),
+          minlength: post?.hasError('minlength'),
+        },
+      };
+      return;
+    }
+    this.#themeService
+      .createTheme(this.themeForm.value)
+      .subscribe((theme: ThemesShape) => {
+        this.#router.navigate(['/themes', theme._id]);
+      });
+  }
+  onCancel() {
+    this.themeForm.reset();
+    this.#router.navigateByUrl('/');
+  }
 }
