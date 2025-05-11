f79a8508f954d271258f4b0ee703832eb28d64c8
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 15:33:27 +0200

    auth guard

---
+++ forum-workshop/src/app/auth.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const isLogged = authService.isLogged;
+  if (isLogged) {
+    return true;
+  } else {
+    return false;
+  }
+};
