b58effc5ca40edb20d3f125185c34bc7f8410661
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 15:33:56 +0200

    added login/register buttons for guests

--- forum-workshop/src/app/main/main.component.ts
+++ forum-workshop/src/app/main/main.component.ts
@@ -4,15 +4,19 @@ import { ThemeService } from '../services/theme.service';
 import { CommonModule } from '@angular/common';
 import { ThemeComponent } from './theme/theme.component';
 import { AsideComponent } from '../aside/aside.component';
+import { HomeComponent } from '../home/home.component';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule, ThemeComponent, AsideComponent],
+  imports: [CommonModule, ThemeComponent, AsideComponent, HomeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
 export class MainComponent implements OnInit {
   #themeService = inject(ThemeService);
+  #authService = inject(AuthService);
+  isUser = this.#authService.isLogged;
   themes: ThemesShape[] = [];
 
   ngOnInit(): void {
