72c870db93415cbbc90294652e9d36f1ccd9dfb2
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 21:01:28 +0200

    set error in toaster if any

--- forum-workshop/src/app/header/header.component.ts
+++ forum-workshop/src/app/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { Observable, Subscription } from 'rxjs';
+import { UserShape } from '../types/User';
+import { ErrorService } from '../services/error.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +11,25 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   #authService = inject(AuthService);
-  isUser: boolean = this.#authService.isLogged;
-  username: string = 'Vladimir';
+  #errorService = inject(ErrorService);
+  #errorSub!: Subscription;
+  username: string = '';
+  errorMessage: string | null = null;
+
+  ngOnInit(): void {
+    this.#authService.isAuthenticated$.subscribe(
+      (data) => (this.username = data.username)
+    );
+    this.#errorSub = this.#errorService.error$.subscribe((message) => {
+      this.errorMessage = message;
+      setTimeout(() => {
+        this.errorMessage = null;
+      }, 5000);
+    });
+  }
+  ngOnDestroy(): void {
+    this.#errorSub.unsubscribe();
+  }
 }
