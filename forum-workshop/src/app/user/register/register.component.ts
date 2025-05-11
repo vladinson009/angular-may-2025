6f9307114858e44112e9ee9d8b1a18e10d21d6c6
Vladimir Gulev <vladinson009@icloud.com>
2025-05-11 15:36:28 +0200

    implemented register form validation

--- forum-workshop/src/app/user/register/register.component.ts
+++ forum-workshop/src/app/user/register/register.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { RouterLink } from '@angular/router';
+import { customEmailValidator } from '../../validators/emailValidator';
+import { CommonModule } from '@angular/common';
+import { matchPassword } from '../../validators/matchPassword';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  #fb = inject(FormBuilder);
+  #authService = inject(AuthService);
 
+  registerForm: FormGroup;
+
+  hasError(field: string, errorCode: string): boolean {
+    const control = this.registerForm.get(field);
+    return !!(control?.hasError(errorCode) && control.dirty && control.touched);
+  }
+  get passwordGroup() {
+    return this.registerForm.get('passwords') as FormGroup;
+  }
+  constructor() {
+    this.registerForm = this.#fb.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      email: ['', [Validators.required, customEmailValidator]],
+      tel: ['', []],
+      passwords: this.#fb.group(
+        {
+          password: ['', [Validators.required, Validators.minLength(5)]],
+          rePassword: ['', [Validators.required]],
+        },
+        {
+          validators: matchPassword,
+        }
+      ),
+    });
+  }
 }
